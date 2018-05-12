bd2f91af80b044e339de414e8caa474ac56b807d
smith <a@b.c>
2018-05-12 18:14:10 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+const App = () => {
+  return (<div className="App">
+      <div className="container">
+      testing
+      </div>
+
+    </div>);
+};
+
+
+export default App;
